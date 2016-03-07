d63fb23544fd6275e8453d939562469bf6883106
Gatsby <xolaoqin@gmail.com>
2016-03-07 11:46:09 +0800

    tab bar
    tab bar 故事版增加新页面

---
+++ Tap/Tap/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  Tap
+//
+//  Created by Gastby on 16/3/7.
+//  Copyright © 2016年 Gastby. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.brownColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
